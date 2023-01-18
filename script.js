c09d0c4ea094617f0d2362d7ae2b9975793db3c3
julius-tamale <juliotamex@proton.me>
2023-01-18 22:23:56 +0200

    project three

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-const currentDate = document.querySelector('.current-date');
-const countDownTimer = document.querySelector('.countdown-timer');
+// Set the date am counting down to
+const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
 
-currentDate.textContent = new Date();
+// Update the count down every 1 second
+let theCount = setInterval(() => {
+
+    // Get today's date and time
+    let todayDate = new Date().getTime();
+
+    // Find the time between now and the countdown date
+    let timeBetween = countDownDate - todayDate;
+    
+    // Time calculations for days, hours, minutes and seconds    
+    var days = Math.floor(timeBetween / (1000 * 60 * 60 * 24)); 
+    var hours = Math.floor((timeBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60));  
+    var seconds = Math.floor((timeBetween % (1000 * 60)) / 1000); 
+
+// Display the result in the element with id="countdown"
+document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+// If the count down is finished, write some text
+if (distance < 0) {
+clearInterval(x);
+document.getElementById("countdown").innerHTML = "Happy New Year!";
+}
+}, 1000);
